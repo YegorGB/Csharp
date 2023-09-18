c32ced950b7404bab6b7b2dc24321cf13c383bc4
Yegor <zosha762@gmail.com>
2023-09-18 13:22:06 +0300

    task66

--- task66/Program.cs
+++ task66/Program.cs
@@ -8,16 +8,8 @@ int m = Convert.ToInt32(Console.ReadLine());
 
 int SumNumbers(int n,int m)
 {
-    if (m>n)
-    {
-        if (m==n||m-1==n) return 0;
-        else return m-1+SumNumbers(n,m-1); 
-    }
-    else
-    {
-        if (m==n||m==n-1) return 0;
-        else return n-1+SumNumbers(n-1,m);
-    }    
+        if (m<n) return 0;
+        else return m+SumNumbers(n,m-1); 
 }
 
 
